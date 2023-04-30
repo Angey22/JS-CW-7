8eb11d1ce6cf68b1655756f35f6acfb92d88fdba
Angey22 <babandval@gmail.com>
2023-04-30 17:19:59 +0300

    Added comments in files: '03-tags' html & js

--- js_repeta_7-1/03-tags.js
+++ js_repeta_7-1/03-tags.js
@@ -1,11 +1,12 @@
-//! Видео - 0:25:40...0:00:00 
+//! Видео - 0:25:40...1:00:20
 
 /*
- * Делегирование
+ * Делегирование:
  * - один из многих
  * - несколько из многих и Set
  */
 
+//! Первый пример кода, переключение - один из многих
 // const tagsContainer = document.querySelector('.js-tags');
 // let selectedTag = null;
 
@@ -15,12 +16,18 @@
 //   if (evt.target.nodeName !== 'BUTTON') {
 //     return;
 //   }
-
+  
+//   // Проверка наличия активной - выбранной кнопки
 //   const currentActiveBtn = document.querySelector('.tags__btn--active');
 
-//   if (currentActiveBtn) {
-//     currentActiveBtn.classList.remove('tags__btn--active');
-//   }
+//   // Условие проверки
+//   // if (currentActiveBtn) {
+//   //   currentActiveBtn.classList.remove('tags__btn--active');
+//   // }
+
+//   // Альтернатива "if"
+//   currentActiveBtn?.classList.remove('tags__btn--active');
+//   // "?." - если "currentActiveBtn" - НЕ "null" (не false), то выполнится "classList.remove", а если - "null" (false), то интерпритатор не будет дальше проверять код.
 
 //   const nextActiveBtn = evt.target;
 //   nextActiveBtn.classList.add('tags__btn--active');
@@ -29,6 +36,8 @@
 //   console.log(selectedTag);
 // }
 
+
+//! Второй пример кода, выбор - несколько из многих
 const tagsContainer = document.querySelector('.js-tags');
 const selectedTags = new Set();
 
@@ -52,3 +61,7 @@ function onTagsContainerClick(evt) {
   btn.classList.toggle('tags__btn--active');
   console.log(selectedTags);
 }
+
+//! Объект "Set" позволяет хранить уникальные значения любого типа, будь то примитивы или ссылки на объекты.
+// Объекты "Set" - это коллекция значений. Значение в Set может встречаться только один раз; оно уникально в коллекции. Вы можете перебирать элементы набора в порядке вставки. Порядок вставки соответствует порядку, в котором каждый элемент был успешно вставлен в коллекцию методом add().
+// Подробнее см. - (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Set).
