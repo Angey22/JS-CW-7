e9b963fc5bcd15a5b8edc865a6e0c1cc833c06f5
Angey22 <babandval@gmail.com>
2023-05-01 19:11:33 +0300

    Added comments in files: '02-filter' html & js

--- js_repeta_7-2/02-filter.js
+++ js_repeta_7-2/02-filter.js
@@ -1,3 +1,6 @@
+//! Видео - 0:17:40...0:34:00
+
+// Исходный массив объектов
 const tech = [
   { label: 'HTML' },
   { label: 'CSS' },
@@ -15,25 +18,33 @@ const tech = [
 ];
 
 /*
+*  Алгоритм работы скрипта:
  * 1. Рендерим разметку элементов списка
  * 2. Слушаем изменение фильтра
  * 3. Фильтруем данные и рендерим новые элементы
  */
 
+// Объект-ссылок
 const refs = {
   list: document.querySelector('.js-list'),
   input: document.querySelector('#filter'),
 };
 
+// "Вешаем" слушатель событий на <input>
 refs.input.addEventListener('input', _.debounce(onFilterChange, 300));
 
+// Создаем переменную - строку с разметкой
 const listItemsMarkup = createListItemsMarkup(tech);
+
+// Вставляем сгенерированную разметку в DOM
 populateList(listItemsMarkup);
 
+// Функция для генерации строки с разметкой
 function createListItemsMarkup(items) {
   return items.map(item => `<li>${item.label}</li>`).join('');
 }
 
+// Функция фильтрации
 function onFilterChange(evt) {
   console.log('INPUT');
   const filter = evt.target.value.toLowerCase();
@@ -46,6 +57,9 @@ function onFilterChange(evt) {
   populateList(listItemsMarkup);
 }
 
+// Функция вставки разметки в DOM
 function populateList(markup) {
   refs.list.innerHTML = markup;
 }
+
+// Библиотека для реализации "пушистого поиска" (поиска, который выполняется при вводе не корректных ключевых слов, в т.ч. введенных с ошибками), который будет выполнять поиск наиболее похожих данных - https://fusejs.io/
